0e080d72b662242c50678e2a9fea5812f36237f7
Malik Muhammad Usman <usmaan.maalik9503@gmail.com>
2023-02-10 12:42:26 +0500

    created price screen

---
+++ lib/screens/price_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  const PriceScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PriceScreen> createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Coin Tracker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: null,
+          )
+        ],
+      ),
+    );
+  }
+}
